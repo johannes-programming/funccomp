a1983f0e30b24ee2da57d1706e4eeb590197e198
Johannes <johannes.programming@gmail.com>
2026-01-19 01:06:55 +0100

    Version 0.3.0

--- src/funccomp/__init__.py
+++ src/funccomp/__init__.py
@@ -1,4 +1,2 @@
-from funccomp.core.BaseComposite import BaseComposite
 from funccomp.core.Composite import Composite
-from funccomp.core.StarComposite import StarComposite
 from funccomp.tests import *
